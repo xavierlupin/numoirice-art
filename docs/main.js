ea276d5143ebe54e4a10fbd29b42d159b92b4052
Xavier Lupin <xavierlupin0418@gmail.com>
2022-06-04 18:55:10 +0800

    feat(docs): add docs

---
+++ docs/main.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', (e) => {
+  new fullpage('#fullpage', {
+    licenseKey: 'ZL9K9-N98W6-TG4H9-1KZL9-GHYMM',
+    // Navigation
+    menu: '#menu',
+    anchors:['home', 'about', 'preview', 'mint', 'link'],
+    slidesNavigation: true,
+    // Scrolling
+    // Accessibility
+    // Design
+    controlArrows: false,
+    // Custom selectors
+    // Events
+    slidesNavigation: true,
+  })
+})
